87d5f390e014cabb8f8c069b65b745cc7c0c54fb
Gabryel Boeira <gabryel392@gmail.com>
2021-02-28 00:53:36 -0300

    Criação e gerenciamento de de módulos
    criado pagina para caso de erro para acesso invalido.
    
    Criado arquivo de configurarão de rotas de acesso ao carregar as informações dos componentes.

---
+++ projeto/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
